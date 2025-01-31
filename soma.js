a70d30d2f47fad371c943b59c94d8ab4ed974eef
guilherme-costa14 <143550872+guilherme-costa14@users.noreply.github.com>
2025-01-31 15:37:32 -0300

    Teste da função soma
    Testando a função soma com vários parâmetros diferentes

---
+++ soma.js
@@ -0,0 +1,6 @@
+function soma(a, b){
+    let total = a + b;
+    return total;
+}
+
+module.exports = soma; //usar a função em outros arquivos
---
+++ soma.test.js
@@ -0,0 +1,18 @@
+const soma = require('./soma');
+
+// test('descrição do teste', função de teste)
+test('soma 1 + 2 deve ser igual a 3', () => {
+    expect(soma(1,2)).toBe(3);
+})
+
+test('soma 3 + 3 deve ser igual a 6', () => {
+    expect(soma(3,3)).toBe(6);
+})
+
+test('soma de -2 + (-2) deve ser igual a -4', () => {
+    expect(soma(-2, -2)).toBe(-4);
+})
+
+test('soma de 0 + 0 deve ser igual a 0', () => {
+    expect(soma(0,0)).toBe(0);
+})
